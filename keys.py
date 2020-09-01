46d527c18324c6b2a9a7a88b0e414aa5079c3d4a
yangyuyou <1084274425@qq.com>
2020-09-01 17:26:38 +0800

    first commit

---
+++ keys.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+ocr key
+@author: 四川光学字符科技有限公司
+"""
+chinese=""" !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}"""
